f421e7655092e46b29864510ea7b83c06464ec78
Neameh Karineh <your.email@example.com>
2024-08-24 19:51:44 +0300

    Add AddToDoItemCommandValidators.

---
+++ src/ToDoApp.Core/Validators/AddToDoItemCommandValidators.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Application.Logic.Commands.ToDoItemCommand;
+
+namespace ToDoApp.Application.Validators
+{
+    public class AddToDoItemCommandValidators : AbstractValidator<AddToDoItemCommand>
+    {
+        public AddToDoItemCommandValidators()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Title could not be empty!");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description could not be empty!");
+            // TO DO : Check the validatione for IsComplete prop.
+        }
+    }
+}
