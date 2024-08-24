0c45abed3a296821dbd2d9a5802ecefac3405b85
Neameh Karineh <your.email@example.com>
2024-08-24 20:17:45 +0300

    Add AddToDoListCommandValidator.

---
+++ src/ToDoApp.Core/Validators/ToDoListValidators/AddToDoListCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Application.Logic.Commands.ToDoListCommand;
+
+namespace ToDoApp.Application.Validators.ToDoListValidators
+{
+    public class AddToDoListCommandValidator: AbstractValidator<AddToDoListCommand>
+    {
+        public AddToDoListCommandValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Title Could not be empty");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description Could not be empty");
+        }
+    }
+}
