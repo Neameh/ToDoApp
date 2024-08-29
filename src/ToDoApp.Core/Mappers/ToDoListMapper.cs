0ba80ceaf4925e8c0d70acfe3ba5ca5b842f3a24
Neameh Karineh <your.email@example.com>
2024-08-29 19:46:26 +0300

    Add ToDoListMapper

---
+++ src/ToDoApp.Core/Mappers/ToDoListMapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Application.DTOs.ToDoList;
+using ToDoApp.Domain.Models;
+
+namespace ToDoApp.Application.Mappers
+{
+    public static class ToDoListMapper
+    {
+        public static ToDoListDTO ToToDoListDTO(this ToDoList ToDoList)
+        {
+            return new ToDoListDTO
+            {
+                Id = ToDoList.ToDoListId,
+                Title = ToDoList.Title,
+                Description = ToDoList.Description
+            };
+        }
+        public static ToDoListRequestDTO ToToDoListRequestDTO(this ToDoList ToDoList)
+        {
+            return new ToDoListRequestDTO
+            {
+                Title = ToDoList.Title,
+                Description = ToDoList.Description,
+            };
+        }
+    }
+}
